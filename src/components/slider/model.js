08d92cbadcba5ff1b6a214f8ed374ed9686ebf6b
Chernenko Viktoriya <4743866@mail.ru>
2019-01-17 20:32:34 +0300

    Новое описание класса SliderModel

--- src/components/slider/model.js
+++ src/components/slider/model.js
@@ -1,9 +1,39 @@
-import SliderView from "./view";
-
 export default class SliderModel {
   constructor (options) {
-    this.el = options.el;
-    this.runner = options.runner;
+    this.val = 0;
+    this.minVal = 0;
+    this.maxVal = 100;
+    this.sliderType = 'single';
+  }
+
+  getValue () {
+    return this.val;
+  }
+
+  getExtremeValues () {
+    return {
+      min: this.minVal,
+      max: this.maxVal
+    }
+  }
+
+  getSliderType () {
+    return this.sliderType;
+  }
+
+  setValue (val) {
+    this.val = val;
+  }
+
+  setExtremeValues(min, max) {
+    this.minVal = min;
+    this.maxVal = max;
+  }
+
+  setSliderType (type) {
+    if (type === 'interval') {
+      this.sliderType = 'interval'
+    }
   }
 
 }
