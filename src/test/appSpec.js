0c65cb7695b6bbc3f565dc33e0a9e4766760557f
Chernenko Viktoriya <4743866@mail.ru>
2019-02-07 21:22:21 +0300

    Написание тестов для класса ScaleView.

--- src/test/appSpec.js
+++ src/test/appSpec.js
@@ -5,6 +5,7 @@ import SliderController from "../components/slider/controller";
 import RunnerView from '../components/slider/runner/RunnerView';
 import TipView from '../components/slider/tip/TipView';
 import TrackView from '../components/slider/track/TrackView';
+import ScaleView from '../components/slider/scale/ScaleView';
 
 // /* VIEW */
 //
@@ -243,303 +244,354 @@ import TrackView from '../components/slider/track/TrackView';
 //   });
 // });
 
+
+
+
 /* RUNNER VIEW */
 
-describe('07 RunnerView', function () {
-  let runnerView,
+// describe('07 RunnerView', function () {
+//   let runnerView,
+//       parent;
+//
+//   beforeEach(function () {
+//     setFixtures('<div class="slider"></div>');
+//     parent = document.getElementsByClassName('slider')[0];
+//     runnerView = new RunnerView({parent: parent});
+//   });
+//
+//   it('Создается экземпляр класса RunnerView', function () {
+//     expect(runnerView).toBeDefined();
+//   });
+//
+// });
+//
+// describe('08 Функция drawRunner', function () {
+//   let runnerView,
+//       parent,
+//       slider,
+//       sliderModel;
+//
+//   beforeEach(function () {
+//     setFixtures('<div class="slider"></div>');
+//     parent = document.getElementsByClassName('slider')[0];
+//     sliderModel = new SliderModel();
+//     slider = new SliderView({el: parent, model: sliderModel});
+//     slider.type = 'interval';
+//     slider.drawSlider();
+//     runnerView = new RunnerView({parent: slider});
+//     runnerView.drawRunner(parent, 2);
+//   });
+//
+//   it('Должна создавать элемент slider__runner внутри элемента slider', function () {
+//     expect($('.slider .slider__runner')).toExist();
+//   });
+//
+//   // it('Создает подсказку, если isTip = true', function () {
+//   //   runnerView.isTip = true;
+//   //   runnerView.drawRunner(parent);
+//   //   expect($('.slider .slider__runner .slider__tip')).toExist();
+//   // });
+//
+// });
+//
+// describe('09 Функция setRunnerPosition', function () {
+//   let runnerView,
+//       parent,
+//       slider,
+//       sliderModel,
+//       runner;
+//
+//   beforeEach(function () {
+//     setFixtures('<div class="slider"></div>');
+//     parent = document.getElementsByClassName('slider')[0];
+//
+//     sliderModel = new SliderModel();
+//
+//     slider = new SliderView({el: parent, model: sliderModel});
+//     slider.type = 'interval';
+//     slider.drawSlider();
+//
+//     runnerView = new RunnerView({parent: slider});
+//     runnerView.drawRunner(parent, 1);
+//     runner = document.getElementsByClassName('slider__runner')[0];
+//   });
+//
+//   it('Ползунку можно задать положение', function () {
+//     runnerView.el.style.width = '50px';
+//     parent.style.width = '350px';
+//     runnerView.setRunnerPosition(2);
+//     expect(runnerView.el.style.left).toEqual('150px');
+//   });
+//
+// });
+//
+// describe('10 Функция setRunnerShiftX', function () {
+//   let runnerView,
+//       parent,
+//       slider,
+//       sliderModel,
+//       runner;
+//
+//   beforeEach(function () {
+//     setFixtures('<div class="slider"></div>');
+//     parent = document.getElementsByClassName('slider')[0];
+//
+//     sliderModel = new SliderModel();
+//
+//     slider = new SliderView({el: parent, model: sliderModel});
+//     slider.type = 'interval';
+//     slider.drawSlider();
+//
+//     runnerView = new RunnerView({parent: slider});
+//     runnerView.drawRunner(parent, 1);
+//     runner = document.getElementsByClassName('slider__runner')[0];
+//   });
+//
+//   it('Сохраняет смещение курсора относительно ползунка', function () {
+//     runnerView.setRunnerShiftX({pageX: 10});
+//     expect(runnerView.shiftX).toEqual(10 - runnerView.el.getBoundingClientRect().left);
+//   });
+//
+// });
+//
+// describe('11 Функция setRunnerShiftY', function () {
+//   let runnerView,
+//       parent,
+//       slider,
+//       sliderModel,
+//       runner;
+//
+//   beforeEach(function () {
+//     setFixtures('<div class="slider"></div>');
+//     parent = document.getElementsByClassName('slider')[0];
+//
+//     sliderModel = new SliderModel();
+//
+//     slider = new SliderView({el: parent, model: sliderModel});
+//     slider.type = 'interval';
+//     slider.drawSlider();
+//
+//     runnerView = new RunnerView({parent: slider});
+//     runnerView.drawRunner(parent, 1);
+//     runner = document.getElementsByClassName('slider__runner')[0];
+//   });
+//
+//   it('Сохраняет смещение курсора относительно ползунка', function () {
+//     runnerView.setRunnerShiftY({pageY: 10});
+//     expect(runnerView.shiftY).toEqual(10 - runnerView.el.getBoundingClientRect().top);
+//   });
+//
+// });
+//
+// describe('12 Функция moveRunner', function () {
+//   let runnerView,
+//       parent,
+//       slider,
+//       sliderModel,
+//       runner;
+//
+//   beforeEach(function () {
+//     setFixtures('<div class="slider"></div>');
+//     parent = document.getElementsByClassName('slider')[0];
+//
+//     sliderModel = new SliderModel();
+//
+//     slider = new SliderView({el: parent, model: sliderModel});
+//     slider.type = 'interval';
+//     slider.drawSlider();
+//
+//     runnerView = new RunnerView({parent: slider});
+//     runnerView.drawRunner(parent, 1);
+//     runner = document.getElementsByClassName('slider__runner')[0];
+//   });
+//
+//   it('Генерирует событие, отправляя коэффициент и тип ползунка', function () {
+//     runnerView.moveRunner({pageX: 30});
+//     expect().toEqual(30);
+//   });
+//
+// });
+//
+// /* TIP VIEW */
+//
+// describe('13 TipView', function () {
+//   let tipView,
+//       parent;
+//
+//   beforeEach(function () {
+//     setFixtures('<div class="slider"></div>');
+//     parent = document.getElementsByClassName('slider')[0];
+//     tipView = new TipView();
+//   });
+//
+//   it('Создается экземпляр класса RunnerView', function () {
+//     expect(tipView).toBeDefined();
+//   });
+//
+// });
+//
+// describe('14 Функция drawTip', function () {
+//   let tipView,
+//       element,
+//       parent,
+//       slider,
+//       sliderModel;
+//
+//   beforeEach(function () {
+//     setFixtures('<div class="slider"></div>');
+//     element = document.getElementsByClassName('slider')[0];
+//     sliderModel = new SliderModel();
+//     slider = new SliderView({el: element, model: sliderModel});
+//     slider.type = 'interval';
+//     slider.drawSlider();
+//     parent = document.getElementsByClassName('slider__runner')[0];
+//     tipView = new TipView();
+//     tipView.drawTip(parent);
+//   });
+//
+//   it('Должна создавать элемент slider__runner внутри элемента slider', function () {
+//     expect($('.slider .slider__runner .slider__tip')).toExist();
+//   });
+//
+// });
+//
+// describe('14 Функция updateTip', function () {
+//   let tipView,
+//       element,
+//       parent,
+//       slider,
+//       sliderModel;
+//
+//   beforeEach(function () {
+//     setFixtures('<div class="slider"></div>');
+//     element = document.getElementsByClassName('slider')[0];
+//     sliderModel = new SliderModel();
+//     slider = new SliderView({el: element, model: sliderModel});
+//     slider.type = 'interval';
+//     slider.drawSlider();
+//     parent = document.getElementsByClassName('slider__runner')[0];
+//     tipView = new TipView();
+//     tipView.drawTip(parent);
+//   });
+//
+//   it('Должна обновлять значение внутри подсказки', function () {
+//     tipView.updateTip(15);
+//     expect($('.slider .slider__runner .slider__tip').text()).toEqual('15');
+//   });
+//
+// });
+//
+//
+// /* TRACK VIEW */
+//
+// describe('15 TrackView', function () {
+//   let trackView;
+//
+//   beforeEach(function () {
+//     trackView = new TrackView();
+//   });
+//
+//   it('Создается экземпляр класса TrackView', function () {
+//     expect(trackView).toBeDefined();
+//   });
+//
+// });
+//
+// describe('16 TrackView', function () {
+//   let trackView,
+//       parent;
+//
+//   beforeEach(function () {
+//     setFixtures('<div class="slider"></div>');
+//     parent = document.getElementsByClassName('slider')[0];
+//     trackView = new TrackView();
+//     trackView.drawTrack(parent);
+//   });
+//
+//   it('Функция drawTrack создает элемент slider__track внутри указанного родителя', function () {
+//     expect($('.slider .slider__track')).toExist();
+//   });
+//
+// });
+//
+// describe('17 Функция animateTrack', function () {
+//   let trackView,
+//       parent,
+//       slider,
+//       sliderModel;
+//
+//   beforeEach(function () {
+//     setFixtures('<div class="slider"></div>');
+//     parent = document.getElementsByClassName('slider')[0];
+//     parent.style.width = '300px';
+//
+//     sliderModel = new SliderModel();
+//     slider = new SliderView({el: parent, model: sliderModel});
+//     slider.drawSlider();
+//
+//     trackView = new TrackView({parent: slider});
+//     trackView.drawTrack(parent);
+//
+//   });
+//
+//   it('Функция animateTrack устанавливает ширину дорожки, если тип слайдера single', function () {
+//     trackView.animateProgress(2);
+//     expect(trackView.trackFull.style.width).toEqual('150px');
+//   });
+//
+//   it('Функция animateTrack устанавливает ширину и отступ дорожки, если тип слайдера interval', function () {
+//     trackView.type = 'interval';
+//     trackView.animateProgress();
+//     expect(trackView.trackFull.style.left).toEqual('');
+//     expect(trackView.trackFull.style.width).toEqual('150px');
+//   });
+//
+// });
+
+
+/* SCALE VIEW */
+
+describe('ScaleView', function () {
+  let scaleView;
+
+  beforeEach(function () {
+    scaleView = new ScaleView();
+  });
+
+  it('Создается экземпляр класса ScaleView', function () {
+    expect(scaleView).toBeDefined();
+  });
+
+});
+
+describe('ScaleView. Функция drawScale', function () {
+  let scaleView,
       parent;
 
   beforeEach(function () {
     setFixtures('<div class="slider"></div>');
-    parent = document.getElementsByClassName('slider')[0];
-    runnerView = new RunnerView({parent: parent});
+    parent = $('.slider');
+
+    scaleView = new ScaleView();
+    scaleView.drawScale(parent, 5, 25, 5);
   });
 
-  it('Создается экземпляр класса RunnerView', function () {
-    expect(runnerView).toBeDefined();
+  it('Создает элемент slider__scale внутри указанного родителя', function () {
+    expect($('.slider .slider__scale')).toExist();
   });
 
-});
-
-describe('08 Функция drawRunner', function () {
-  let runnerView,
-      parent,
-      slider,
-      sliderModel;
-
-  beforeEach(function () {
-    setFixtures('<div class="slider"></div>');
-    parent = document.getElementsByClassName('slider')[0];
-    sliderModel = new SliderModel();
-    slider = new SliderView({el: parent, model: sliderModel});
-    slider.type = 'interval';
-    slider.drawSlider();
-    runnerView = new RunnerView({parent: slider});
-    runnerView.drawRunner(parent, 2);
+  it('Создает элементы slider__scale-item', function () {
+    expect($('.slider .slider__scale .slider__scale-item')).toExist();
   });
 
-  it('Должна создавать элемент slider__runner внутри элемента slider', function () {
-    expect($('.slider .slider__runner')).toExist();
+  it('Количество элементов slider__scale-item определяется в зависимости от min, max и размера шага', function () {
+    expect($('.slider .slider__scale .slider__scale-item').length).toEqual(5);
   });
 
-  // it('Создает подсказку, если isTip = true', function () {
-  //   runnerView.isTip = true;
-  //   runnerView.drawRunner(parent);
-  //   expect($('.slider .slider__runner .slider__tip')).toExist();
-  // });
-
-});
-
-describe('09 Функция setRunnerPosition', function () {
-  let runnerView,
-      parent,
-      slider,
-      sliderModel,
-      runner;
-
-  beforeEach(function () {
-    setFixtures('<div class="slider"></div>');
-    parent = document.getElementsByClassName('slider')[0];
-
-    sliderModel = new SliderModel();
-
-    slider = new SliderView({el: parent, model: sliderModel});
-    slider.type = 'interval';
-    slider.drawSlider();
-
-    runnerView = new RunnerView({parent: slider});
-    runnerView.drawRunner(parent, 1);
-    runner = document.getElementsByClassName('slider__runner')[0];
-  });
-
-  it('Ползунку можно задать положение', function () {
-    runnerView.el.style.width = '50px';
-    parent.style.width = '350px';
-    runnerView.setRunnerPosition(2);
-    expect(runnerView.el.style.left).toEqual('150px');
-  });
-
-});
-
-describe('10 Функция setRunnerShiftX', function () {
-  let runnerView,
-      parent,
-      slider,
-      sliderModel,
-      runner;
-
-  beforeEach(function () {
-    setFixtures('<div class="slider"></div>');
-    parent = document.getElementsByClassName('slider')[0];
-
-    sliderModel = new SliderModel();
-
-    slider = new SliderView({el: parent, model: sliderModel});
-    slider.type = 'interval';
-    slider.drawSlider();
-
-    runnerView = new RunnerView({parent: slider});
-    runnerView.drawRunner(parent, 1);
-    runner = document.getElementsByClassName('slider__runner')[0];
-  });
-
-  it('Сохраняет смещение курсора относительно ползунка', function () {
-    runnerView.setRunnerShiftX({pageX: 10});
-    expect(runnerView.shiftX).toEqual(10 - runnerView.el.getBoundingClientRect().left);
-  });
-
-});
-
-describe('11 Функция setRunnerShiftY', function () {
-  let runnerView,
-      parent,
-      slider,
-      sliderModel,
-      runner;
-
-  beforeEach(function () {
-    setFixtures('<div class="slider"></div>');
-    parent = document.getElementsByClassName('slider')[0];
-
-    sliderModel = new SliderModel();
-
-    slider = new SliderView({el: parent, model: sliderModel});
-    slider.type = 'interval';
-    slider.drawSlider();
-
-    runnerView = new RunnerView({parent: slider});
-    runnerView.drawRunner(parent, 1);
-    runner = document.getElementsByClassName('slider__runner')[0];
-  });
-
-  it('Сохраняет смещение курсора относительно ползунка', function () {
-    runnerView.setRunnerShiftY({pageY: 10});
-    expect(runnerView.shiftY).toEqual(10 - runnerView.el.getBoundingClientRect().top);
-  });
-
-});
-
-describe('12 Функция moveRunner', function () {
-  let runnerView,
-      parent,
-      slider,
-      sliderModel,
-      runner;
-
-  beforeEach(function () {
-    setFixtures('<div class="slider"></div>');
-    parent = document.getElementsByClassName('slider')[0];
-
-    sliderModel = new SliderModel();
-
-    slider = new SliderView({el: parent, model: sliderModel});
-    slider.type = 'interval';
-    slider.drawSlider();
-
-    runnerView = new RunnerView({parent: slider});
-    runnerView.drawRunner(parent, 1);
-    runner = document.getElementsByClassName('slider__runner')[0];
-  });
-
-  it('Генерирует событие, отправляя коэффициент и тип ползунка', function () {
-    runnerView.moveRunner({pageX: 30});
-    expect().toEqual(30);
-  });
-
-});
-
-/* TIP VIEW */
-
-describe('13 TipView', function () {
-  let tipView,
-      parent;
-
-  beforeEach(function () {
-    setFixtures('<div class="slider"></div>');
-    parent = document.getElementsByClassName('slider')[0];
-    tipView = new TipView();
-  });
-
-  it('Создается экземпляр класса RunnerView', function () {
-    expect(tipView).toBeDefined();
-  });
-
-});
-
-describe('14 Функция drawTip', function () {
-  let tipView,
-      element,
-      parent,
-      slider,
-      sliderModel;
-
-  beforeEach(function () {
-    setFixtures('<div class="slider"></div>');
-    element = document.getElementsByClassName('slider')[0];
-    sliderModel = new SliderModel();
-    slider = new SliderView({el: element, model: sliderModel});
-    slider.type = 'interval';
-    slider.drawSlider();
-    parent = document.getElementsByClassName('slider__runner')[0];
-    tipView = new TipView();
-    tipView.drawTip(parent);
-  });
-
-  it('Должна создавать элемент slider__runner внутри элемента slider', function () {
-    expect($('.slider .slider__runner .slider__tip')).toExist();
-  });
-
-});
-
-describe('14 Функция updateTip', function () {
-  let tipView,
-      element,
-      parent,
-      slider,
-      sliderModel;
-
-  beforeEach(function () {
-    setFixtures('<div class="slider"></div>');
-    element = document.getElementsByClassName('slider')[0];
-    sliderModel = new SliderModel();
-    slider = new SliderView({el: element, model: sliderModel});
-    slider.type = 'interval';
-    slider.drawSlider();
-    parent = document.getElementsByClassName('slider__runner')[0];
-    tipView = new TipView();
-    tipView.drawTip(parent);
-  });
-
-  it('Должна обновлять значение внутри подсказки', function () {
-    tipView.updateTip(15);
-    expect($('.slider .slider__runner .slider__tip').text()).toEqual('15');
-  });
-
-});
-
-
-/* TRACK VIEW */
-
-describe('15 TrackView', function () {
-  let trackView;
-
-  beforeEach(function () {
-    trackView = new TrackView();
-  });
-
-  it('Создается экземпляр класса TrackView', function () {
-    expect(trackView).toBeDefined();
-  });
-
-});
-
-describe('16 TrackView', function () {
-  let trackView,
-      parent;
-
-  beforeEach(function () {
-    setFixtures('<div class="slider"></div>');
-    parent = document.getElementsByClassName('slider')[0];
-    trackView = new TrackView();
-    trackView.drawTrack(parent);
-  });
-
-  it('Функция drawTrack создает элемент slider__track внутри указанного родителя', function () {
-    expect($('.slider .slider__track')).toExist();
-  });
-
-});
-
-describe('17 Функция animateTrack', function () {
-  let trackView,
-      parent,
-      slider,
-      sliderModel;
-
-  beforeEach(function () {
-    setFixtures('<div class="slider"></div>');
-    parent = document.getElementsByClassName('slider')[0];
-    parent.style.width = '300px';
-
-    sliderModel = new SliderModel();
-    slider = new SliderView({el: parent, model: sliderModel});
-    slider.drawSlider();
-
-    trackView = new TrackView({parent: slider});
-    trackView.drawTrack(parent);
-
-  });
-
-  it('Функция animateTrack устанавливает ширину дорожки, если тип слайдера single', function () {
-    trackView.animateProgress(2);
-    expect(trackView.trackFull.style.width).toEqual('150px');
-  });
-
-  it('Функция animateTrack устанавливает ширину и отступ дорожки, если тип слайдера interval', function () {
-    trackView.type = 'interval';
-    trackView.animateProgress();
-    expect(trackView.trackFull.style.left).toEqual('');
-    expect(trackView.trackFull.style.width).toEqual('150px');
+  it('Элемент slider__scale-item содержит корректное значение', function () {
+    expect($('.slider .slider__scale .slider__scale-item').eq(0).text()).toEqual('5');
+    expect($('.slider .slider__scale .slider__scale-item').eq(1).text()).toEqual('10');
+    expect($('.slider .slider__scale .slider__scale-item').eq(2).text()).toEqual('15');
   });
 
 });
